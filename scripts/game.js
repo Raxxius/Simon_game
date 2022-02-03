5e8d9419e08db4be7004bbff65e71f527b14efbb
Raxxius <Raxxman@gmail.com>
2022-02-03 19:08:17 +0000

    updated lightOn and associated tests

--- scripts/game.js
+++ scripts/game.js
@@ -2,8 +2,43 @@ let game = {
     currentGame: [],
     score: 0,
     playerMoves: [],
-    choices: [],
+    choices: ["button1", "button2", "button3", "button4"],
 }
 
+function newGame() {
+    game.score = 0;
+    game.currentGame = [];
+    game.playerMoves = [];
+    showScore();
+    addTurn();
+}
 
-module.exports = { game }
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    showTurns();
+}
+
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+}
+
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add(circ + "light");
+    setTimeout(function () {
+        document.getElementById(circ).classList.remove(circ + "light");
+    }, 400);
+}
+
+function showTurns() {
+    game.turnNumber = 0;
+    let turns = setInterval(function () {
+        lightsOn(game.currentGame[game.turnNumber]);
+        game.turnNumber++;
+        if (game.turnNumber >= game.currentGame.length) {
+            clearInterval(turns);
+        }
+    }, 800);
+}
+
+module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns };
