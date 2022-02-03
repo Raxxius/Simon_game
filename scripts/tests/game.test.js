ae9aaa1acc9f507faba0ca1b15b5f50d0605d45b
Raxxius <Raxxman@gmail.com>
2022-02-03 16:22:33 +0000

    add 3rd and 4th tests

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -3,11 +3,13 @@
  */
 
 
+const { expect } = require("@jest/globals");
+const { test } = require("picomatch");
 const { game } = require("../game");
 
-beforeAll()) => {
+beforeAll(() => {
     let fs = require("fs");
-    let fileContents = fs.readFileSync("index.html". "utf-8");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
     document.open();
     document.write(fileContents);
     document.close();
@@ -16,5 +18,14 @@ beforeAll()) => {
 describe("game object contains correct keys", () => {
     test("score key exists", () => {
         expect("score" in game).toBe(true);
+    });
+    test("currentGame key exists", () => {
+        expect("currentGame" in game).toBe(true);
+    });
+    test("playerMoves key exists", () => {
+        expect("playerMoves" in game).toBe(true);
+    });
+    test("choices key exists", () => {
+        expect("choices" in game).toBe(true);
     });
 });
